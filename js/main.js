f421a3758c5364ce7dea3b330fcb503b15008a0d
Joshua McGowan <usaf0316@gmail.com>
2020-11-15 21:55:20 -0600

    Ok im done

--- js/main.js
+++ js/main.js
@@ -4,20 +4,25 @@ function getJson() {
     .then(response => response.json())
     .then(data => {
         console.log(data)
-    let high_temp = (((data.main.temp_max)-273.15) * (9/5) ) + 32 + " " + ('F')
-    let low_temp = (((data.main.temp_min)-273.15) * (9/5) ) + 32 + " " + ('F')
+    let high_temp = Math.round((((data.main.temp_max)-273.15) * (9/5) )) + 32 + " " +('\u00B0') + ('F')
+    let low_temp = Math.round((((data.main.temp_min)-273.15)) * (9/5) ) + 32 + " " + ('\u00B0') + ('F')
     let forecast_temp = (data.weather[0].description)
-    // let forecast = data.weather[0].description
+    let humidity = data.main.humidity + ('%')
+
     let display_temp = document.createElement('td')
     let display1_temp = document.createElement('td')
     let display2_temp = document.createElement('td')
+    let display3_temp = document.createElement('td')
 
     display_temp.innerHTML = high_temp
-    display2_temp.innerHTML = low_temp
     display1_temp.innerHTML = forecast_temp
+    display2_temp.innerHTML = low_temp
+    display3_temp.innerHTML = humidity
+
     document.querySelector(`#mac`).append(display_temp)
-    document.querySelector(`#and`).append(display2_temp)
     document.querySelector(`#cheese`).append(display1_temp)
+    document.querySelector(`#and`).append(display2_temp)
+    document.querySelector('#withgravy').append(display3_temp)
 
     })
 }
